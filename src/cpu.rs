bfaff774ef15007fef3b62eae0a277693517a1b6
Rudy Banerjee <rudyb2001@gmail.com>
2026-04-21 11:48:29 -0700

    Update cpu.rs
    Added basic memory and functions for testing

--- src/cpu.rs
+++ src/cpu.rs
@@ -5,6 +5,8 @@ struct CPU {
     Y: u8,      // Y register
     SR: u8,     // Status register
     SP: u8,     // Stack pointer
+
+    Memory: &Memory,
 }
 
 const SR_N: u8 = 0b1000_0000;   // Negative
@@ -15,6 +17,27 @@ const SR_I: u8 = 0b0000_0100;   // Interrupt (IRQ disable)
 const SR_Z: u8 = 0b0000_0010;   // Zero
 const SR_C: u8 = 0b0000_0000;   // Carry
 
+#[derive(Default)]
+struct Memory {
+    data: [u8; 0x10000],
+}
+
+impl Memory {
+    func get(&self, address: u16) -> u8 {
+        self.data[address]
+    }
+
+    func set(&mut self, address: u16, data: u8) {
+        self.data[address] = data;
+    }
+}
+
 impl CPU {
-    func 
+    func ADC(&self, inputs: &[u8]) {
+
+    }
+
+    func AM_Acc(&self, inputs: &[u8]) -> u8 {
+
+    }
 }
